257eb7a56534b1ca2ed443b16a72f971284f20d7
Matthew-Bourgeois <matthewbourgeois42@gmail.com>
2025-05-10 13:00:08 -0500

    Add SinLog route and navigation link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import Todo from './pages/Todo';
-import Contact from './pages/Contact';
+
 import Home from './pages/Home';
 import About from './pages/About';
+import Todo from './pages/Todo';
+import Contact from './pages/Contact';
 import SevenDeadlySins from './pages/SevenDeadlySins';
+import SinLog from './pages/SinLog'; // ✅ Import the new page
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
             <li><Link to="/todo">Todos</Link></li>
             <li><Link to="/contact">Contact</Link></li>
             <li><Link to="/seven-deadly-sins">Seven Deadly Sins</Link></li>
+            <li><Link to="/sin-log">Sin Log</Link></li> {/* ✅ Add Sin Log Link */}
           </ul>
         </nav>
         <Routes>
@@ -27,6 +30,7 @@ function App() {
           <Route path="/todo" element={<Todo />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/seven-deadly-sins" element={<SevenDeadlySins />} />
+          <Route path="/sin-log" element={<SinLog />} /> {/* ✅ SinLog route */}
         </Routes>
       </div>
     </Router>
