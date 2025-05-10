ce14c0baa986797ab19bc90ef0fe027f324317c8
Matthew-Bourgeois <matthewbourgeois42@gmail.com>
2025-05-10 15:42:20 -0500

    Updates

---
+++ precache-manifest.b8a94d1a28fc5205214c7949bbcd5861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb67e862052c17755e597af48d13d7f",
+    "url": "/spiritual-addiction-tracker/index.html"
+  },
+  {
+    "revision": "ce61f3c4f65ba34a7a9f",
+    "url": "/spiritual-addiction-tracker/static/css/main.dd935e86.chunk.css"
+  },
+  {
+    "revision": "41f1f00412374d509e00",
+    "url": "/spiritual-addiction-tracker/static/js/2.07f38d17.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/spiritual-addiction-tracker/static/js/2.07f38d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1174baadac5e2da9deeb",
+    "url": "/spiritual-addiction-tracker/static/js/3.9bb14dc1.chunk.js"
+  },
+  {
+    "revision": "ce61f3c4f65ba34a7a9f",
+    "url": "/spiritual-addiction-tracker/static/js/main.0f6f1a72.chunk.js"
+  },
+  {
+    "revision": "f78e0f0360d92a7b0086",
+    "url": "/spiritual-addiction-tracker/static/js/runtime-main.641baed3.js"
+  }
+]);
